94060880760c8dd1835bf6bd3b8f2639d66f487d
pianattero <mariapianattero@gmail.com>
2023-09-24 01:21:14 +0200

    create basic structure

---
+++ src/views/misc/Home/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="hero">
+        <img className="glasses" src="../../../../public/images/glasses.png" />
+        <img
+          className="turistearte"
+          src="../../../../public/images/turistearte.png"
+        />
+        <h3>Web en construcción...</h3>
+      </div>
+    </>
+  );
+};
