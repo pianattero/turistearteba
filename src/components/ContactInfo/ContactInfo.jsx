64bbad5247921aeae7f75a736fc50b0e0dbd1bb3
pianattero <mariapianattero@gmail.com>
2023-09-24 01:54:13 +0200

    added contact info space

---
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -0,0 +1,9 @@
+export const ContactInfo = () => {
+  return (
+    <>
+      <div className="contact">
+        <h5>Contact Info</h5>
+      </div>
+    </>
+  );
+};
--- src/views/misc/Home/Home.jsx
+++ src/views/misc/Home/Home.jsx
@@ -1,15 +1,25 @@
 import "./Home.css";
 
+import { ContactInfo } from "../../../components/ContactInfo/ContactInfo";
+
 export const Home = () => {
   return (
     <>
-      <div className="hero">
-        <img className="glasses" src="../../../../public/images/glasses.png" />
-        <img
-          className="turistearte"
-          src="../../../../public/images/turistearte.png"
-        />
-        <h3>Web en construcción...</h3>
+      <div className="home">
+        <div className="imgs">
+          <img
+            className="glasses"
+            src="../../../../public/images/glasses.png"
+          />
+          <img
+            className="turistearte"
+            src="../../../../public/images/turistearte.png"
+          />
+          <h3>Web en construcción...</h3>
+        </div>
+        <div className="fixed-bottom text-center p-4 bg-light">
+          <ContactInfo />
+        </div>
       </div>
     </>
   );
